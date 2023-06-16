e72821cc0390a49802df3dcfaa2cca1f0e034efd
Harshit Nigam <hrshtngm@amazon.com>
2023-06-16 09:36:12 +0530

    A simple carousel react component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { Slider } from './components/slider';
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function App() {
+
+  //get slides from backend api endpoint
+  const [slides, setSlides] = useState([]);
+  const getSlides = async () => {
+    const response = await axios.get(`http://localhost:8080/fetchData`);
+    console.log(response.data);
+    setSlides(response.data);
+  };
+  useEffect(() => {
+    getSlides();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Slider slides={slides}/>
   );
 }
 
---
+++ src/components/slider.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import "./styles.css";
+const responsive = {
+    desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: 4,
+        slidesToSlide: 4 // optional, default to 1.
+    },
+    tablet: {
+        breakpoint: { max: 1024, min: 768 },
+        items: 3,
+        slidesToSlide: 3 // optional, default to 1.
+    },
+    mobile: {
+        breakpoint: { max: 767, min: 464 },
+        items: 2,
+        slidesToSlide: 1 // optional, default to 1.
+    }
+};
+export const Slider = ({slides}) => {
+    return (
+        <div className="parent">
+            <Carousel
+                responsive={responsive}
+                swipeable={true}
+                draggable={true}
+                showDots={true}
+                infinite={true}
+                partialVisible={false}
+                dotListClass="custom-dot-list-style"
+            >
+                {slides.map((slide) => {
+                    return (
+                        <div className="slider">
+                            <img src={slide.image} alt="movie"/>
+                        </div>
+                    );
+                })}
+            </Carousel>
+        </div>
+    );
+};
